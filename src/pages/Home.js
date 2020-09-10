f27e2351d31e516167a6d61f8e19f8a0ad2813fe
Steven Hofheins <stevenisinsane@gmail.com>
2020-09-10 16:49:32 -0600

    added buttons to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,28 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className='flex'>
+      <div className='text-center w-full text-2xl '>
+        React - Tailwind CSS - Theme Changer
+      </div>
+      <div className='flex text-center mt-8'>
         <div className='w-1/4'>
           <button className='px-1 py-1 rounded border border-black bg-white'>
             Light
           </button>
         </div>
         <div className='w-1/4'>
-          <button className='px-1 py-1 rounded border border-black bg-black text white'>
+          <button className='px-1 py-1 rounded border border-black bg-black text-white'>
             Dark
+          </button>
+        </div>
+        <div className='w-1/4'>
+          <button className='px-1 py-1 rounded border border-green-300 bg-green-300 text-white'>
+            Pastel
+          </button>
+        </div>
+        <div className='w-1/4'>
+          <button className='px-1 py-1 rounded border border-pink-500 bg-pink-500 text-white'>
+            Neon
           </button>
         </div>
       </div>
