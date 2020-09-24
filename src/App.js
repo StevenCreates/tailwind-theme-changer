2b77b487908c1cf93b95a3a5062a3fc627408c30
Steven Hofheins <stevenisinsane@gmail.com>
2020-09-24 13:26:16 -0600

    added app back in

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./assets/main.css";
 import Home from "./pages/Home";
+import ThemeContext from "./utilities/ThemeContext";
 
 function App() {
   return (
-    <div>
+    <ThemeContext>
       <BrowserRouter>
         <Switch>
           <Route path='/' exact component={Home} />
         </Switch>
       </BrowserRouter>
-    </div>
+    </ThemeContext>
   );
 }
 
